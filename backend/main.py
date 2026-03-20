006454c4f5696ecbcf6e11bb6747b5371ac92632
niemczyt <niemczyt@robopong.local>
2026-03-20 16:07:47 +0100

    fix: BackgroundTask import z starlette

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Set
 
 from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
-from fastapi.background import BackgroundTask
+from starlette.background import BackgroundTask
 from fastapi.responses import FileResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 
